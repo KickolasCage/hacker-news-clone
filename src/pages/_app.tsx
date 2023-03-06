fab7dd414e3b3af3bfd900910591e404691b3639
The Wrath of God <st.grigelevich@gmail.com>
2023-03-06 23:03:57 +0000

    Initial commit; implements pages for all major categories of posts with working comment trees and user description pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
+import Error from "@/components/Error";
+
+import "@/styles/globals.css";
+import styles from "@/styles/Home.module.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-[750px] mx-auto">
+      <Navigation />
+      <div className="bg-white rounded-md py-5">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
